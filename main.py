84b9d3ad198c0f0da56cf120440d4e5fb3bac5bb
Eduardo Lira <eduardolirainfo@gmail.com>
2021-01-31 02:05:20 +0000

    break

--- main.py
+++ main.py
@@ -8,3 +8,25 @@ c = 5
 while c != 0:
     print(c)
     c -= 1
+
+print("-----------------------------------")
+print("Explicito é melhor do que implicito")
+print("-----------------------------------")
+
+c = 5
+
+while c:
+    print(c)
+    c -= 1
+
+print("-----------------------------------")
+print("-------------BREAK-------------")
+
+while True:
+    response = input()
+    if int(response) % 7 == 0: 
+        break
+    else:
+        print("Não é divisivel por 7")
+
+        
